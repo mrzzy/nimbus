97c21d783ddc9874dca2514f54c6daa14383b77a
Zhu Zhanyan <program.nom@gmail.com>
2021-07-07 14:03:30 +0800

    Setup Terraform for Wasabi Cloud Storage (#1)
    * chore: Config git to ignore terraform files
    
    * chore: Config git to ignore generated tag files
    
    * feat: Add wasabi providers for eu-central-1 and us-west-2 regions
    
    * feat: Add access_key and secret_key input variables pass key pair for auth with wasabi
    
    * feat: Add regional s3 endpoints for wasabi providers
    
    * feat(wasabi): Add wasabi bucket to store terraform state for nimbus project
    
    * refactor(wasabi): Replace wasabi bucket to store terraform state with terraform cloud remote backend
    
    * feat(ci) Add CI/CD Github Actions workflows to deploy wasabi terraform
    
    * feat(ci): Only trigger terraform wasabi workflow when terraform/wasabi changes

---
+++ terraform/wasabi/main.tf
@@ -0,0 +1,48 @@
+#
+# nimbus
+# terraform deploy for wasabi storage
+#
+
+## Providers ##
+terraform {
+  backend "remote" {
+    organization = "mrzzy-co"
+    workspaces {
+      name = "nimbus"
+    }
+  }
+  required_providers {
+    wasabi = {
+      source  = "k-t-corp/wasabi"
+      version = "4.1.1"
+    }
+  }
+}
+
+provider "wasabi" {
+  alias  = "eu-central-1"
+  region = "eu-central-1"
+
+  endpoints {
+    sts = "https://sts.wasabisys.com"
+    iam = "https://iam.wasabisys.com"
+    s3  = "https://s3.eu-central-1.wasabisys.com"
+  }
+
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+provider "wasabi" {
+  alias  = "us-west-1"
+  region = "us-west-1"
+
+  endpoints {
+    sts = "https://sts.wasabisys.com"
+    iam = "https://iam.wasabisys.com"
+    s3  = "https://s3.us-west-1.wasabisys.com"
+  }
+
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
