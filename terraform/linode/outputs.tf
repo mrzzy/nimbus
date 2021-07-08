5023e10df0f283293def32fa6b6bb32f3c445924
Zhu Zhanyan <program.nom@gmail.com>
2021-07-08 11:21:02 +0800

    feat(terraform): Config linode terraform to output kubeconfig file of deployed LKE cluster

--- terraform/linode/outputs.tf
+++ terraform/linode/outputs.tf
@@ -13,3 +13,9 @@ output "k8s_singapore_status" {
   value       = linode_lke_cluster.singapore.status
   description = "Status of Linode LKE Cluster in Singapore"
 }
+
+output "k8s_singapore_kubeconfig" {
+  value       = linode_lke_cluster.singapore.kubeconfig
+  description = "Base64 encoded kubeconfig used to connect to the created LKE cluster"
+  sensitive   = true
+}
