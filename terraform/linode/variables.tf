44ae85078315580b7d72577b977f1ed7eaee98e8
Zhu Zhanyan <program.nom@gmail.com>
2021-09-04 09:50:22 +0800

    Provide access to internal LKE services via Bastion Host & Internal Ingress (#33)
    * feat(linode): add terraform module to deploy wireguard server on linode
    
    * fix(linode): fix filepath to setup_wireguard script not relative to module path
    
    * feat(terraform): deploy wireguard server as bastion host to Linode LKE
    
    * fix(wasabi): fix linode instance  having two dashes in label
    
    This is disallowed by the linode API
    
    * fix(terraform): Fix setup_wireguard script not creating required /etc/wireguard/ dir
    
    * refactor(wasabi): pass wireguard private key via UDF to prevent key from being templated in stackscript in plaintext
    
    * fix(terraform): fix UDF in setup_wireguard script missing label field in tag
    
    * feat(terraform): add ssh key to bastion to provide debug ssh access to host
    
    * feat(terraform): output ip of bastion host
    
    * fix(terraform): remove redudant ppa breaking setup_wireguard stackscript
    
    * fix(terraform): fix typo in wireguard config template 'AlllowIPs' should be' AllowedIPs'
    
    * feat(terraform): attach bastion server to private network so it can be used to access instances via LAN
    
    * fix(terraform): fix bastion server not forwarding packets
    
    * feat(terraform): add firewall to Linode LKE nodes to limit internet access of K8s NodePort services
    
    * fix(terraform): allow all private traffic to ensure LKE worker can be accessed by master & bastion
    
    * feat(k8s): add kustomize deploy for internal ingress with nodeport service & 'nginx-internal' ingress class
    
    * refactor(k8s): refactor internal ingress deploy to remove name prefix as it breaks deployed ingress
    
    * fix(k8s): namespace cluster scoped resources to prevent collision with existing ingress-nginx deploy
    
    * fix(k8s): fix RBAC role to allow the controller to update configmap to perform leader election
    
    * fix(k8s): disable webhook validation for ingress due ca issues caused by multiple ingress controllers
    
    * feat(k8s): expose ArgoCD deployment via internal ingress via host argocd.mrzzy.co
    
    * feat(k8s): add ability to create port fowards on bastion host
    
    This facilates the accessing NodePort services via their standard port
    where their NodePort does not line up (ie access internal ingress via
    80/443 where their actual ports are 3XXXX).
    
    * feat(k8s): create port forwards on bastion host to forward 80/443 to internal ingress nodeport service
    
    * fix(k8s): Remove ip6tables rules from forwarding to fix ipv4 addresses being fed to ipv6 rules
    
    * fix(k8s): fix typo in dest port in iptables dnat route routing http to internal ingress
    
    * feat(k8s): expose argo workflows via internal ingress on host argo.mrzzy.co
    
    * feat(k8s): expose kibana via internal ingress on host kibana.mrzzy.co
    
    * feat(k8s): expose grafana via internal ingress on host grafana.mrzzy.co
    
    * feat(terraform): add firewall to bastion host to limit incoming traffic to wireguard and ssh

--- terraform/linode/variables.tf
+++ terraform/linode/variables.tf
@@ -14,3 +14,18 @@ variable "prefix" {
   type        = string
   description = "Prefix to attach to labels of created resources"
 }
+
+variable "tags" {
+  type        = set(string)
+  description = "List of unique tags to apply to deployed resources"
+
+  default = [
+    "terraform"
+  ]
+}
+
+variable "bastion_wireguard_private_key" {
+  type        = string
+  sensitive   = true
+  description = "Private Key used by the Wireguard VPN exposed by the bastion host"
+}
