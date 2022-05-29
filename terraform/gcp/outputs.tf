ce602090bb7e6089bf75a67e996c3fa8e0b9ce8e
Zhu Zhanyan <program.nom@gmail.com>
2022-05-29 17:03:35 +0800

    Add Terraform / GH Action Workflows to Deploy WARP Dev VM (#37)
    * build(gcp): add google cloud platform provider to terraform
    
    * build(terraform): config terraform cloud to store state for gcp infra
    
    * feat(terraform): add terraform to deploy warp box dev vm with 10G disk for storage
    
    * fix(terraform): warp box vm image has to be specified as string
    
    * chore(terraform): enroll mrzzy's ssh key into GCE metadata for sshing VMs
    
    * refactor(terraform): attach wrap VM to custom VPC for default deny firewall
    
    Using the current default network has implied firewall rules for remote access.
    https://cloud.google.com/vpc/docs/firewalls#default_firewall_rules
    
    * feat(terraform): add GCP VPC firewall rule to allow ssh traffic to VM tagged allow-ssh
    
    * feat(terraform): output warp vm external ip once provisioned
    
    * build(terraform): peg google provider to versions >=4.22.0, <4.23.0
    
    * ci(terraform): add ci pipeline to apply terraform deployment on GCP
    
    * chore: terraform fmt main.tf
    
    * chore: add pre commit hook to check for terraform fmt
    
    * bump: terraform version support to versions <1.3.0
    
    * bump(terraform): bump google terraform provider to version 4.22.0
    
    * ci(terraform): rename terraform-gcp-ci workflow to apply-terraform-gcp
    
    to make way for future destory-terraform-gcp workflow
    
    * ci(terraform): trigger apply-terraform-gcp workflow on manual workflow_dispatch event
    
    * feat(terraform): allow warp vm to be conditionally deployed based on has_warp_vm var
    
    * ci(terraform): allow users to control warp vm deploy via workflow run menu
    
    * ci(terraform): add automated workflow to cleanup costly gcp resources
    
    * ci(terraform): enable CI workflows condition checks after testing

---
+++ terraform/gcp/outputs.tf
@@ -0,0 +1,10 @@
+#
+# Nimbus
+# Terraform Deployment: Google Cloud Platform
+# Output variables
+#
+
+output "warp_box_ip" {
+  description = "Publicly accessible IP of the WARP development VM."
+  value       = google_compute_instance.wrap_vm[*].network_interface[0].access_config[0].nat_ip
+}
