10326690ed06a57862c8c2ba51db83efaceff6e2
Zhu Zhanyan <program.nom@gmail.com>
2022-06-17 06:25:07 +0800

    refactor(terraform): Split WARP VM Infra into its own Terraform Module (#43)
    * refactor(terraform): split out warp vm deploy into warp_vm.tf from main.tf
    
    * refactor(terraform): refactor gcp extract warp vm deploy into own module
    
    * fix(terraform): missing 'moved' tombstone for warp disk
    
    requried to prevent terraform from deleting warp disk.
    
    * refactor(terraform): nest termplates within child modules
    
    * fix(terraform): warp vm external ip no longer exposed as output var
    
    * fix(terraform): warp disk size no longer configurable from root module.

--- terraform/gcp/outputs.tf
+++ terraform/gcp/outputs.tf
@@ -1,10 +1,11 @@
 #
 # Nimbus
 # Terraform Deployment: Google Cloud Platform
-# Output variables
+# Output
 #
 
-output "warp_box_ip" {
-  description = "Publicly accessible IP of the WARP development VM."
-  value       = google_compute_instance.wrap_vm[*].network_interface[0].access_config[0].nat_ip
+
+output "warp_vm_ip" {
+  description = "Publicly accessible IP of the WARP VM."
+  value       = module.warp_vm.external_ip
 }
