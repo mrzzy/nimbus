f2ff67b1a4b6f690493cdd854628917c8cbb6d25
Zhu Zhanyan <program.nom@gmail.com>
2022-06-17 18:35:13 +0800

    fix(terraform): infering warp_vm_ip output as sensitive

--- terraform/gcp/outputs.tf
+++ terraform/gcp/outputs.tf
@@ -7,5 +7,5 @@
 
 output "warp_vm_ip" {
   description = "Publicly accessible IP of the WARP VM."
-  value       = module.warp_vm.external_ip
+  value       = nonsensitive(module.warp_vm.external_ip)
 }
