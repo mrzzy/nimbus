18727d86dec0f23de12dc9e05e8f23376307b67d
Zhu Zhanyan <program.nom@gmail.com>
2022-06-18 07:15:33 +0800

    fix(terraform): missing null check before passing arg to nonsensitive()

--- terraform/gcp/outputs.tf
+++ terraform/gcp/outputs.tf
@@ -4,8 +4,11 @@
 # Output
 #
 
+locals {
+  warp_ip = module.warp_vm.external_ip
+}
 
 output "warp_vm_ip" {
   description = "Publicly accessible IP of the WARP VM."
-  value       = nonsensitive(module.warp_vm.external_ip)
+  value       = local.warp_ip == null ? null : nonsensitive(local.warp_ip)
 }
