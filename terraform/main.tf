3e2bd4d49e97648df0c920c7a54549a4baa6aff0
Zhu Zhanyan <program.nom@gmail.com>
2022-06-18 16:43:33 +0800

    chore(terraform): add moved tombstones for resourced moved to child modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,3 +52,24 @@ module "warp_vm" {
   allow_ssh_tag = local.allow_ssh_tag
   disk_size_gb  = var.warp_disk_size_gb
 }
+
+# Tombstones for resources moved into child modules
+moved {
+  from = google_compute_disk.warp_disk
+  to   = module.warp_vm.google_compute_disk.warp_disk
+}
+
+moved {
+  from = google_compute_network.sandbox
+  to   = module.gce.google_compute_network.sandbox
+}
+
+moved {
+  from = google_compute_firewall.sandbox
+  to   = module.gce.google_compute_firewall.sandbox
+}
+
+moved {
+  from = google_compute_project_metadata_item.ssh_keys
+  to   = module.gce.google_compute_project_metadata_item.ssh_keys
+}
