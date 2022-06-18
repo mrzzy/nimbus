7f1e70a0ce0983e9b2597c73e6e73295a91f0857
Zhu Zhanyan <program.nom@gmail.com>
2022-06-18 16:44:38 +0800

    fix(terraform): missing 'with' in var description

--- terraform/modules/gce/variables.tf
+++ terraform/modules/gce/variables.tf
@@ -6,5 +6,5 @@
 
 variable "allow_ssh_tag" {
   type        = string
-  description = "Allow SSH traffic on port 22 to instances this GCE Metadata tag."
+  description = "Allow SSH traffic on port 22 to instances with this GCE Metadata tag."
 }
