d0a8ed8789abdca413a92739f8c1f4a6f7a77dab
Zhu Zhanyan <program.nom@gmail.com>
2022-06-24 14:28:08 +0800

    feat(terraform): Allow HTTP Access to WARP VM by IP Range (#47)
    * ci(terraform): add enable http web term & cidr range inputs to apply terraform workflow
    
    * revert: "feat(terraform): allow warp web tls to be set via warp_web_tls_key & warp_web_tls_cert vars"
    
    This reverts commit 6d9b6aa2daef87e05e0947eb391fd6992086c295.
    
    * ci(terraform): standardize 'WARP VM:' prefix for all warp inputs in ci workflow
    
    * feat(terraform): pass ci workflow inputs to terraform variables
    
    * feat(terraform): add support for setting allowed ip cidr range in ingress_allows var
    
    * feat(terraform): allow http for warp vm's http terminal if enabled
    
    * ci(terraform): fix typo
    
    * fix(terraform): rectify port no. type to string

--- terraform/modules/gce/variables.tf
+++ terraform/modules/gce/variables.tf
@@ -5,10 +5,12 @@
 #
 
 variable "ingress_allows" {
-  type        = map(number)
+  type        = map(list(string))
   description = <<-EOF
-  List of ingress allow rules to create the firewall as map of <tag> = <port>.
-  This allows ingress traffic from the internet to the specified port on
-  GCE instances tagged with the specified GCE Metadata tag.
+  List of ingress allow rules to create the firewall.
+  Expressed as map of <tag> = [<cidr>, <port>].
+
+  This allows ingress traffic from the IPs in the CIDR range to the specified
+  port on GCE instances tagged with the specified GCE Metadata tag.
   EOF
 }
