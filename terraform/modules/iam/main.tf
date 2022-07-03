86045e5a1a2ce19454e19a520ee180ef11cc6779
Zhu Zhanyan <program.nom@gmail.com>
2022-07-03 11:12:31 +0800

    feat(terraform): import warp-packer-builder service account into iam module

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -13,7 +13,7 @@ terraform {
 }
 
 # Service Accounts
-# service account for terraform itself.
+# service account for terraform itself to apply cloud infrastructure on GCP
 resource "google_service_account" "terraform" {
   project     = var.project
   account_id  = "nimbus-ci-terraform"
@@ -32,6 +32,16 @@ resource "google_service_account" "warp" {
   display_name = "Service account for WARP VM instance(s)."
 }
 
+# service account for WARP VM image Packer builder
+resource "google_service_account" "warp_builder" {
+  project     = var.project
+  account_id  = "warp-packer-builder"
+  description = <<-EOF
+  Service Account for WARP VM's Packer builder to build WARP VM images on
+  GCE VM instances.
+  EOF
+}
+
 # IAM Role Bindings
 # project editor role
 resource "google_project_iam_binding" "editor" {
@@ -39,5 +49,22 @@ resource "google_project_iam_binding" "editor" {
   role    = "roles/editor"
   members = [
     google_service_account.terraform.email
+  ]
+}
+
+# compute instance admin
+resource "google_project_iam_binding" "vm_admin" {
+  project = var.project
+  role    = "roles/compute.instanceAdmin.v1"
+  members = [
+    google_service_account.warp_builder.email
+  ]
+}
+
+resource "google_project_iam_binding" "service_account_user" {
+  project = var.project
+  role    = "roles/iam.serviceAccountUser"
+  members = [
+    google_service_account.warp_builder.email
   ]
 }
