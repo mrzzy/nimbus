ef6ef44f63080683adbf3b2bcfc6e3a1f232a6f7
Zhu Zhanyan <program.nom@gmail.com>
2022-07-03 11:33:24 +0800

    fix(terraform): removed moved blocks for project metadata key ssh-keys

--- terraform/moved.tf
+++ terraform/moved.tf
@@ -19,8 +19,3 @@ moved {
   from = google_compute_firewall.sandbox
   to   = module.gce.google_compute_firewall.sandbox
 }
-
-moved {
-  from = google_compute_project_metadata_item.ssh_keys
-  to   = module.gce.google_compute_project_metadata_item.ssh_keys
-}
