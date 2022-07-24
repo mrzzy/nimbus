9c34c183d7cb1127e382174bd0210ab722c4d430
Zhu Zhanyan <program.nom@gmail.com>
2022-07-24 10:17:40 +0800

    feat(terraform): mount btrfs subvolume to persist warp VM docker volumes

--- terraform/modules/linode/k8s/main.tf
+++ terraform/modules/linode/k8s/main.tf
@@ -9,6 +9,10 @@ terraform {
       source  = "linode/linode"
       version = ">=1.28.0, <1.29.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.12.1"
+    }
   }
 }
 
@@ -22,4 +26,17 @@ resource "linode_lke_cluster" "main" {
     type  = var.machine_type
     count = 1
   }
+}
+
+# Configure Terraform provider for Kubernetes to access K8s on Linode
+locals {
+  kubeconfig = yamldecode(base64decode(linode_lke_cluster.main.kubeconfig))
+}
+provider "kubernetes" {
+  host = linode_lke_cluster.main.api_endpoints[0]
+
+  token = one(local.kubeconfig["users"])["user"]["token"]
+  cluster_ca_certificate = base64decode(
+    one(local.kubeconfig["clusters"])["cluster"]["certificate-authority-data"]
+  )
 }
