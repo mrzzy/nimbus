265d1e0f895283617eb262a04b3a4c1d92e191f2
Zhu Zhanyan <program.nom@gmail.com>
2022-07-26 17:44:29 +0800

    feat(terraform): add media.mrzzy.co dns route for jellyfin media server

--- terraform/main.tf
+++ terraform/main.tf
@@ -175,7 +175,8 @@ module "dns" {
   domain = "mrzzy.co"
   routes = merge({
     # dns routes for services served by k8s's ingress
-    "auth" : module.k8s.ingress_ip # oauth2-proxy oauth callbacks / login page
+    "auth" : module.k8s.ingress_ip,  # oauth2-proxy oauth callbacks / login page
+    "media" : module.k8s.ingress_ip, # jellyfin media server
     },
     # only create dns route for WARP VM if its deployed
     var.has_warp_vm ? { "warp" : local.warp_ip } : {},
