e26e52f0c7a4c2fb88b52d994832439bd70cceee
Zhu Zhanyan <program.nom@gmail.com>
2022-07-31 09:44:57 +0800

    feat(k8s): Add Ingress-nginx Ingress & OAuth2-Proxy authentication (#55)
    * ci(k8s): add dry-run-k8s ci job to dry run apply k8s manifests cluster-side
    
    * ci(k8s): refactor link-k8s to dry run on lke cluster instead
    
    * chore: config git to ignore .kubeconfig
    
    * feat(k8s): deploy ingress-nginx ingress controller & oauth2-proxy helm chart
    
    * chore: git ignore helm charts dir under k8s
    
    * feat(terraform): add dns route auth.mrzzy.co to oauth2-proxy
    
    * feat(k8s): expose oauth2-proxy via ingress on auth.mrzzy.co route
    
    * build(terraform): add terraform provider for kubernetes
    
    * feat: use TLS cert procured by terraform ACME provider in oauth2-proxy k8s ingress
    
    * chore(git): ignore .*.env files used to build k8s secrets
    
    * feat(k8s): patch oauth2-proxy to obtain secrets from env
    
    * refactor(k8s): extract trailored ingress config into ingress patch
    
    * fix(terraform): encoding of tls credentials & revert double base64 encode
    
    * feat(k8s): config oauth-proxy to use github provider & restrict user emails
    
    * fix(k8s): whitelist *.mrzzy.co domains to be redirected to after oauth2
    
    * build(terraform): update terraform providers
    
    * ci: update name of linode token secret
    
    * ci: update kubectl dry run command to allow kustomize to use helm
    
    * ci: bump linode cli used in workflow to 5.22.0
    
    * fix(k8s): secret.env path in kustomize
    
    * refactor: remove detect-secret's baseline file as its not used
    
    * ci(k8s): hook up github actions secrets needed by kustomize to template secrets
    
    * ci: add step to diff manifests with current k8s infra
    
    * refactor(k8s): remove commented out oauth2-proxy config.
    
    * docs(terraform): clarify tls_cert with comment
    
    * docs(terraform): move WARP DNS route comment down to the correct position
    
    * ci: handle case kubectl returns 1 status code when it detects changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -149,12 +149,23 @@ resource "google_compute_project_metadata_item" "ssh_keys" {
   value = "mrzzy:ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBrfd982D9iQVTe2VecUncbgysh/XsZb4YyOhCSSAAtr mrzzy"
 }
 
+# Linode: LKE Cluster
 module "k8s" {
   source = "./modules/linode/k8s"
   region = local.linode_region
 
   machine_type = "g6-standard-2" # 2vCPU, 2GB
   n_workers    = 1
+
+  # TLS credentials to add to the cluster as K8s secrets.
+  tls_certs = {
+    "${local.domain_slug}-tls" = {
+      "cert" = module.tls_cert.full_chain_cert,
+    }
+  }
+  tls_keys = {
+    "${local.domain_slug}-tls" = module.tls_cert.private_key,
+  }
 }
 
 # Linode: DNS zone & routes for mrzzy.co domain
@@ -162,6 +173,11 @@ module "dns" {
   source = "./modules/linode/dns"
 
   domain = "mrzzy.co"
-  # only create dns route for WARP VM if its deployed
-  routes = var.has_warp_vm ? { "warp" : local.warp_ip } : {}
+  routes = merge({
+    # dns routes for services served by k8s's ingress
+    "auth" : module.k8s.ingress_ip # oauth2-proxy oauth callbacks / login page
+    },
+    # only create dns route for WARP VM if its deployed
+    var.has_warp_vm ? { "vm.warp" : local.warp_ip } : {},
+  )
 }
