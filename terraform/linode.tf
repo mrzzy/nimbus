4cf8f63ff223f2542b8a13a57495a7adb735f0ce
Zhu Zhanyan <program.nom@gmail.com>
2022-08-11 21:59:59 +0800

    refactor(terraform): split main.tf into cloud provider specific files

---
+++ terraform/linode.tf
@@ -0,0 +1,55 @@
+#
+# Nimbus
+# Linode Terraform Deployment
+#
+
+locals {
+  domain      = "mrzzy.co"
+  domain_slug = replace(local.domain, ".", "-")
+
+  # Linode deploy region
+  linode_region = "ap-south" # singapore
+}
+
+# Linode Cloud
+provider "linode" {}
+
+# Linode: LKE Cluster
+module "k8s" {
+  source = "./modules/linode/k8s"
+  region = local.linode_region
+
+  machine_type = "g6-standard-2" # 2vCPU, 2GB
+  n_workers    = 1
+
+  # TLS credentials to add to the cluster as K8s secrets.
+  tls_certs = {
+    "${local.domain_slug}-tls" = {
+      "cert" = module.tls_cert.full_chain_cert,
+    }
+  }
+  tls_keys = {
+    "${local.domain_slug}-tls" = module.tls_cert.private_key,
+  }
+}
+
+# Linode: DNS zone & routes for mrzzy.co domain
+locals {
+  warp_ip = (
+    # TODO(mrzzy): this conditional seems to do nothing, can we remove it?
+    module.warp_vm.external_ip == null ? null : module.warp_vm.external_ip
+  )
+}
+module "dns" {
+  source = "./modules/linode/dns"
+
+  domain = "mrzzy.co"
+  routes = merge({
+    # dns routes for services served by k8s's ingress
+    "auth" : module.k8s.ingress_ip,  # oauth2-proxy oauth callbacks / login page
+    "media" : module.k8s.ingress_ip, # jellyfin media server
+    },
+    # only create dns route for WARP VM if its deployed
+    var.has_warp_vm ? { "warp" : local.warp_ip } : {},
+  )
+}
