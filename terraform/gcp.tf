a6d5f7adb0bacd8a9c01387fc68b88f03b3b6472
Zhu Zhanyan <program.nom@gmail.com>
2022-08-14 09:23:59 +0800

    feat(terraform): Refactor & Add Backblaze B2 Terraform Provider (#58)
    * refactor(terraform): split main.tf into cloud provider specific files
    
    * refactor(terraform): remove useless ternary conditonal
    
    * build(terraform): add terraform provider for backblaze b2
    
    * refactor(terraform): use domain local var

---
+++ terraform/gcp.tf
@@ -0,0 +1,94 @@
+#
+# Nimbus
+# Google Cloud Deployment
+#
+
+locals {
+  # GCP project
+  gcp_project_id = "mrzzy-sandbox"
+
+  # GCE tags for firewall rules
+  allow_ssh_tag       = "allow-ssh"
+  allow_https_tag     = "allow-https"
+  warp_allow_http_tag = "warp-allow-http"
+  warp_allow_dev_tag  = "warp-allow-dev"
+
+  # mrzzy's SSH public key
+  ssh_public_key = "mrzzy:ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBrfd982D9iQVTe2VecUncbgysh/XsZb4YyOhCSSAAtr mrzzy"
+}
+provider "google" {
+  project = local.gcp_project_id
+  region  = "asia-southeast1" # singapore
+  zone    = "asia-southeast1-c"
+}
+
+# GCP: Shared IAM resources
+module "iam" {
+  source = "./modules/gcp/iam"
+
+  project = local.gcp_project_id
+}
+
+# GCP: Shared VPC network VM instances reside on
+module "vpc" {
+  source = "./modules/gcp/vpc"
+
+  ingress_allows = merge(
+    {
+      "ssh" = {
+        "tag"  = local.allow_ssh_tag,
+        "cidr" = "0.0.0.0/0",
+        "port" = "22",
+      },
+      "https" = {
+        "tag"  = local.allow_https_tag,
+        "cidr" = "0.0.0.0/0",
+        "port" = "443",
+      },
+      "warp-http" = {
+        "tag"  = local.warp_allow_http_tag,
+        "cidr" = var.warp_allow_ip,
+        "port" = "80",
+      },
+    },
+    # create allow rules for WARP VM development ports
+    {
+      for port in compact(split(",", var.warp_allow_ports)) :
+      "warp-dev-${trim(port, " ")}" => {
+        "tag"  = local.warp_allow_dev_tag,
+        "cidr" = var.warp_allow_ip,
+        "port" = trim(port, " "),
+      }
+    }
+  )
+}
+
+# GCP: Deploy WARP Box development VM on GCP
+# https://github.com/mrzzy/warp
+module "warp_vm" {
+  source = "./modules/gcp/warp_vm"
+
+  enabled      = var.has_warp_vm
+  image        = var.warp_image
+  machine_type = var.warp_machine_type
+  tags = concat(
+    [
+      local.allow_ssh_tag,
+      local.allow_https_tag,
+      local.warp_allow_dev_tag,
+    ],
+    # allow http for warp vm's http terminal if enabled
+    var.warp_http_terminal ? [local.warp_allow_http_tag] : [],
+  )
+  disk_size_gb = var.warp_disk_size_gb
+
+  web_tls_cert   = module.tls_cert.full_chain_cert
+  web_tls_key    = module.tls_cert.private_key
+  ssh_public_key = local.ssh_public_key
+}
+
+# GCP: enroll project-wide ssh key for ssh access to VMs
+resource "google_compute_project_metadata_item" "ssh_keys" {
+  key   = "ssh-keys"
+  value = local.ssh_public_key
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -3,25 +3,6 @@
 # Terraform Deployment
 #
 
-locals {
-  domain      = "mrzzy.co"
-  domain_slug = replace(local.domain, ".", "-")
-
-  # GCP project
-  gcp_project_id = "mrzzy-sandbox"
-  # GCE tags for firewall rules
-  allow_ssh_tag       = "allow-ssh"
-  allow_https_tag     = "allow-https"
-  warp_allow_http_tag = "warp-allow-http"
-  warp_allow_dev_tag  = "warp-allow-dev"
-
-  # Linode deploy region
-  linode_region = "ap-south" # singapore
-
-  # mrzzy's SSH public key
-  ssh_public_key = "mrzzy:ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBrfd982D9iQVTe2VecUncbgysh/XsZb4YyOhCSSAAtr mrzzy"
-}
-
 terraform {
   required_version = ">=1.2.0, <1.3.0"
 
@@ -38,6 +19,10 @@ terraform {
       source  = "vancluever/acme"
       version = ">=2.9.0, <2.10.0"
     }
+    b2 = {
+      source  = "Backblaze/b2"
+      version = "0.8.1"
+    }
   }
 
   # terraform cloud workspace to store terraform state
@@ -49,30 +34,10 @@ terraform {
     }
   }
 }
-
-# Google Cloud Platform
-provider "google" {
-  project = local.gcp_project_id
-  region  = "asia-southeast1" # singapore
-  zone    = "asia-southeast1-c"
-}
-
-# Linode Cloud
-provider "linode" {}
-
-
 # Lets Encrypt ACME TLS certificate issuer
 provider "acme" {
   server_url = var.acme_server_url
 }
-
-# GCP: Shared IAM resources
-module "iam" {
-  source = "./modules/gcp/iam"
-
-  project = local.gcp_project_id
-}
-
 # Issue trusted wildcard TLS certificate for domain via ACME
 module "tls_cert" {
   source = "./modules/linode/tls_acme"
@@ -81,104 +46,5 @@ module "tls_cert" {
   domains     = ["*.${local.domain}"]
 }
 
-# GCP: Shared VPC network VM instances reside on
-module "vpc" {
-  source = "./modules/gcp/vpc"
-
-  ingress_allows = merge(
-    {
-      "ssh" = {
-        "tag"  = local.allow_ssh_tag,
-        "cidr" = "0.0.0.0/0",
-        "port" = "22",
-      },
-      "https" = {
-        "tag"  = local.allow_https_tag,
-        "cidr" = "0.0.0.0/0",
-        "port" = "443",
-      },
-      "warp-http" = {
-        "tag"  = local.warp_allow_http_tag,
-        "cidr" = var.warp_allow_ip,
-        "port" = "80",
-      },
-    },
-    # create allow rules for WARP VM development ports
-    {
-      for port in compact(split(",", var.warp_allow_ports)) :
-      "warp-dev-${trim(port, " ")}" => {
-        "tag"  = local.warp_allow_dev_tag,
-        "cidr" = var.warp_allow_ip,
-        "port" = trim(port, " "),
-      }
-    }
-  )
-}
-
-# GCP: Deploy WARP Box development VM on GCP
-# https://github.com/mrzzy/warp
-module "warp_vm" {
-  source = "./modules/gcp/warp_vm"
-
-  enabled      = var.has_warp_vm
-  image        = var.warp_image
-  machine_type = var.warp_machine_type
-  tags = concat(
-    [
-      local.allow_ssh_tag,
-      local.allow_https_tag,
-      local.warp_allow_dev_tag,
-    ],
-    # allow http for warp vm's http terminal if enabled
-    var.warp_http_terminal ? [local.warp_allow_http_tag] : [],
-  )
-  disk_size_gb = var.warp_disk_size_gb
-
-  web_tls_cert   = module.tls_cert.full_chain_cert
-  web_tls_key    = module.tls_cert.private_key
-  ssh_public_key = local.ssh_public_key
-}
-locals {
-  warp_ip = (
-    module.warp_vm.external_ip == null ? null : module.warp_vm.external_ip
-  )
-}
-# GCP: enroll project-wide ssh key for ssh access to VMs
-resource "google_compute_project_metadata_item" "ssh_keys" {
-  key   = "ssh-keys"
-  value = "mrzzy:ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBrfd982D9iQVTe2VecUncbgysh/XsZb4YyOhCSSAAtr mrzzy"
-}
-
-# Linode: LKE Cluster
-module "k8s" {
-  source = "./modules/linode/k8s"
-  region = local.linode_region
-
-  machine_type = "g6-standard-2" # 2vCPU, 2GB
-  n_workers    = 1
-
-  # TLS credentials to add to the cluster as K8s secrets.
-  tls_certs = {
-    "${local.domain_slug}-tls" = {
-      "cert" = module.tls_cert.full_chain_cert,
-    }
-  }
-  tls_keys = {
-    "${local.domain_slug}-tls" = module.tls_cert.private_key,
-  }
-}
-
-# Linode: DNS zone & routes for mrzzy.co domain
-module "dns" {
-  source = "./modules/linode/dns"
-
-  domain = "mrzzy.co"
-  routes = merge({
-    # dns routes for services served by k8s's ingress
-    "auth" : module.k8s.ingress_ip,  # oauth2-proxy oauth callbacks / login page
-    "media" : module.k8s.ingress_ip, # jellyfin media server
-    },
-    # only create dns route for WARP VM if its deployed
-    var.has_warp_vm ? { "warp" : local.warp_ip } : {},
-  )
-}
+# Backblaze B2 Cloud Storage provider
+provider "b2" {}
