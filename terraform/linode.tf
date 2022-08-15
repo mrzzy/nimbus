5714675217fd7d907d43f20af4a12da9eb32af91
Zhu Zhanyan <program.nom@gmail.com>
2022-08-15 22:37:47 +0800

    fix(terraform): s3 endpoint used should use 'us-west-004' region

--- terraform/linode.tf
+++ terraform/linode.tf
@@ -7,7 +7,7 @@ locals {
   # Linode deploy region
   linode_region = "ap-south" # singapore
   # Backblaze B2 S3-compatible regional endpoint
-  b2_endpoint = "https://s3.us-west-001.backblazeb2.com"
+  b2_endpoint = "https://s3.us-west-004.backblazeb2.com"
 }
 
 # Linode Cloud
