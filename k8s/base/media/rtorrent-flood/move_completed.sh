7ca52542eb9a84f09844733cfb77e7e0eb871415
Zhu Zhanyan <program.nom@gmail.com>
2022-08-18 12:10:01 +0800

    feat(media): add move_completed.sh script to completed downloads to /media

---
+++ k8s/base/media/rtorrent-flood/move_completed.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+#
+# Nimbus
+# Media Streaming Service
+# Move Completed Script
+#
+
+set -e -o pipefail
+
+# Usage: move_completed.sh <DOWNLOAD> <SRCDIR> <DESTDIR>
+# Move the complete download the the given path to the given destination dir.
+#
+# Maintains the relative path of the download with the given source dir in the
+# destination dir when moving files. Echos the path of the moved download once moved.
+#
+# Arguments:
+#   <DOWNLOAD>    Absolute path to the downloads to move located within <SRCDIR>.
+#   <SRCDIR>      Absolute path to source directory that contains the downloaded files.
+#   <DESTDIR>     Absolute path to the destination directory to move the downloaded files to.
+#
+
+# check no. of args
+if [[ $# -ne 3 ]]; then
+  echo "move_completed.sh: Expected 3 arguments" >&2
+  exit 1
+fi
+
+# parse args: remove trailing slashes from dirs
+DOWNLOAD="$1"
+SRCDIR=$(printf "$2" | sed -e "s:/$::")
+DESTDIR=$(printf "$3" | sed -e "s:/$::")
+
+# perform move of downloads
+DESTPATH=$(printf "$DOWNLOAD" | sed -e "s:$SRCDIR:$DESTDIR:")
+DESTDIR=$(dirname "$DESTPATH")
+mkdir -p "$DESTPATH"
+mv -f "$DOWNLOAD" "$DESTPATH"
+
+# echo moved path
+if [[ -d ]]; then
+  echo "$DESTPATH"
+else
+  echo "$DESTDIR"
+fi
