26718ee11d08c3681f31d5f76b7b15460d4df533
Zhu Zhanyan <program.nom@gmail.com>
2022-09-09 06:29:35 +0800

    refactor(terraform): Add Cloudflare as CDN & DNS Provider (#63)
    * refactor(terraform): port linode dns module to cloudflare dns module
    
    * refactor(terraform): switch dns provider from linode to cloudflare
    
    * refactor(terraform): allow dns provider to be configured via var.dns_provider
    
    decouples tls_acme from linode.
    
    * refactor(terraform): switch TLS acme dns provider from linode -> cloudflare
    
    * feat(terraform): enable HTTP/2 & enforce HTTPS on cloudflare for security

---
+++ terraform/cloudflare.tf
@@ -0,0 +1,44 @@
+#
+# Nimbus
+# Terraform Deployment
+# Cloudflare
+#
+
+locals {
+  cf_account_id = "3a282e33c95eef3f663f0fc3e028b6df"
+  warp_ip       = module.warp_vm.external_ip
+}
+
+# Cloudflare: expects access token provided via $CLOUDFLARE_API_TOKEN env var
+provider "cloudflare" {}
+
+# Cloudflare DNS: dns zone & routes for domain
+module "dns" {
+  source = "./modules/cloudflare/dns"
+
+  account_id = local.cf_account_id
+  domain     = local.domain
+  routes = merge({
+    # dns routes for services served by k8s's ingress
+    "auth" : module.k8s.ingress_ip,  # oauth2-proxy oauth callbacks / login page
+    "media" : module.k8s.ingress_ip, # jellyfin media server
+    },
+    # only create dns route for WARP VM if its deployed
+    var.has_warp_vm ? { "warp" : local.warp_ip } : {},
+  )
+}
+
+# Cloudflare settings for domain
+data "cloudflare_zone" "domain" {
+  account_id = local.cf_account_id
+  name       = local.domain
+}
+resource "cloudflare_zone_settings_override" "domain" {
+  zone_id = data.cloudflare_zone.domain.zone_id
+  settings {
+    http2 = "on"
+    # enforce HTTPS for security
+    always_use_https         = "on"
+    automatic_https_rewrites = "on"
+  }
+}
