c2c05bd0dc53445394b37f7f646427040f1fbe42
Zhu Zhanyan <program.nom@gmail.com>
2022-09-09 06:29:35 +0800

    refactor(terraform): Add Cloudflare as CDN & DNS Provider (#63)
    * refactor(terraform): port linode dns module to cloudflare dns module
    
    * refactor(terraform): switch dns provider from linode to cloudflare
    
    * refactor(terraform): allow dns provider to be configured via var.dns_provider
    
    decouples tls_acme from linode.
    
    * refactor(terraform): switch TLS acme dns provider from linode -> cloudflare
    
    * feat(terraform): enable HTTP/2 & enforce HTTPS on cloudflare for security

---
+++ terraform/modules/cloudflare/dns/main.tf
@@ -0,0 +1,30 @@
+#
+# Nimbus
+# Terraform Deployment
+# Cloudflare DNS
+#
+
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "3.22.0"
+    }
+  }
+}
+
+resource "cloudflare_zone" "domain" {
+  account_id = var.account_id
+  zone       = var.domain
+  plan       = "free" # pricing plan
+}
+
+resource "cloudflare_record" "route" {
+  for_each = var.routes
+  zone_id  = cloudflare_zone.domain.id
+
+  type  = "A"
+  ttl   = 60 # 1 min
+  name  = each.key
+  value = each.value
+}
