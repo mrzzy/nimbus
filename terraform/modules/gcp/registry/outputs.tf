4a77af4da740c9be15a64fc235c5296c700bd19c
Zhu Zhanyan <program.nom@gmail.com>
2022-09-11 17:26:05 +0800

    docs(terraform): clarify use of repo_prefix output of registry module

--- terraform/modules/gcp/registry/outputs.tf
+++ terraform/modules/gcp/registry/outputs.tf
@@ -5,6 +5,11 @@
 #
 
 output "repo_prefix" {
-  description = "Repository prefix in tags of containers pushed to this repository."
+  description = <<-EOF
+  Repository prefix in containers tags pushed to this Container Registry.
+
+  Containers should be tagged & pushed  with '<repo_prefix>/<NAME>' to target this
+  Containers Registry.
+  EOF
   value       = "${google_artifact_registry_repository.repo.location}-docker.pkg.dev/${google_artifact_registry_repository.repo.project}/${google_artifact_registry_repository.repo.repository_id}"
 }
