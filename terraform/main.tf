e9bb1d8fb7b1aa8bf0a1ac9f6bc14062c7b8fcbf
Zhu Zhanyan <program.nom@gmail.com>
2022-09-23 08:29:30 +0000

    feat(terraform): add b2 bucket to store loki logs

--- terraform/main.tf
+++ terraform/main.tf
@@ -73,6 +73,11 @@ resource "b2_bucket" "media" {
   bucket_name = "${local.domain_slug}-media"
   bucket_type = "allPrivate"
 }
+# bucket for storing logs for Monitoring (Loki)
+resource "b2_bucket" "logs" {
+  bucket_name = "${local.domain_slug}-logs"
+  bucket_type = "allPrivate"
+}
 # App Key to auth S3 CSI for provisioning B2 Bucket backed K8s Persistent Volumes
 resource "b2_application_key" "k8s_csi" {
   key_name = "k8s-csi"
