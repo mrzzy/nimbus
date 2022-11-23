e0ccf3cf8af648878552ac850a1065cd50d1df22
Zhu Zhanyan <program.nom@gmail.com>
2022-11-23 07:48:41 +0000

    feat(proxy-gae): add template.py to template nginx.conf from proxy spec

---
+++ docker/proxy-gae/template.py
@@ -0,0 +1,52 @@
+#
+# Nimbus
+# Proxy GAE
+# Template Nginx Conf
+#
+
+import re, os
+from argparse import ArgumentParser
+from textwrap import dedent
+from typing import Dict
+from jinja2 import Environment, FileSystemLoader, PackageLoader
+
+
+def parse_proxy(spec_str: str) -> Dict[str, str]:
+    """Parse the given proxy spec into a dictionary of routes to target"""
+    spec = dict([route.split("=") for route in spec_str.split()])
+    if "/health" in spec:
+        raise ValueError("'/health' route is reserved for health checks.")
+    return spec
+
+
+if __name__ == "__main__":
+    # parse command line arguments
+    parser = ArgumentParser(description="Template nginx.conf for Proxy GAE")
+    parser.add_argument(
+        "proxy_spec",
+        type=parse_proxy,
+        help=dedent(
+            """
+        Specify routes that should be proxied in the format:
+        '/<ROUTE>=<TARGET> [/<ROUTE>=<TARGET2> ...]'
+        Example: '/proxy=https://proxy.me' will proxy all requests sent to
+        '/proxy/target/url' to 'https://proxy.me/target/url'
+        """
+        ),
+    )
+    parser.add_argument(
+        "--output-path",
+        type=str,
+        help="Path to write the templated nginx.conf",
+        default="/etc/nginx/nginx.conf",
+    )
+    args = parser.parse_args()
+
+    # template nginx config
+    env = Environment(loader=FileSystemLoader(os.getcwd()))
+    nginx_conf = env.get_template("nginx.conf.jinja2").render(
+        proxy_spec=args.proxy_spec
+    )
+
+    with open(args.output_path, "w") as f:
+        f.write(nginx_conf)
