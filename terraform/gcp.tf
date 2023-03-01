1ef9e7fbc2473d2026a8ccea8cf65bb89119f29f
Zhu Zhanyan <program.nom@gmail.com>
2023-03-01 10:17:04 +0800

    build(terraform): Update WARP VM Terraform module. (#237)
    build(terraform): bump revision of warp vm TF module
    
    Co-authored-by: mrzzy <mrzzy@users.noreply.github.com>

--- terraform/gcp.tf
+++ terraform/gcp.tf
@@ -95,7 +95,7 @@ module "registry" {
 # GCP: Deploy WARP Box development VM on GCP
 # https://github.com/mrzzy/warp
 module "warp_vm" {
-  source = "github.com/mrzzy/warp//deploy/terraform/gcp_vm?ref=77ba0db48c456c57378a3588d7ce81cc7e838b88"
+  source = "github.com/mrzzy/warp//deploy/terraform/gcp_vm?ref=6e53ee47fb507d97213da6cbe26d8a731c718166"
 
   enabled      = var.has_warp_vm
   image        = var.warp_image
