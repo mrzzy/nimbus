dfce50247bf8714f578a2e92c87d48e30c66409e
Zhu Zhanyan <program.nom@gmail.com>
2023-03-13 18:21:22 +0800

    feat(terraform): use dns cname records to route warp vm via proxy gae

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -7,6 +7,7 @@
 locals {
   cf_account_id = "3a282e33c95eef3f663f0fc3e028b6df"
   warp_ip       = module.warp_vm.external_ip
+  proxy_host    = module.proxy_service.hostname
 }
 
 # Cloudflare: expects access token provided via $CLOUDFLARE_API_TOKEN env var
@@ -25,9 +26,14 @@ module "dns" {
     "monitor" : module.k8s.ingress_ip, # Grafana monitoring
     "library" : module.k8s.ingress_ip, # EBook Library
     },
-    # only create dns route for WARP VM if its deployed
-    var.has_warp_vm ? { "warp" : local.warp_ip } : {},
+    # create direct dns route for WARP VM if its deployed without proxy
+    (var.has_warp_vm && !var.has_gae_proxy) ? { "warp" : local.warp_ip } : {},
   )
+  # create dns route to WARP VM via proxy if deployed with proxy
+  cnames = (var.has_warp_vm && var.has_gae_proxy) ? merge(
+    { "warp" : local.proxy_host },
+    { for match in regexall("(?P<host>[\\w\\.]+)=") : match.host => local.proxy_host },
+  ) : {}
 }
 
 # Cloudflare settings for domain
