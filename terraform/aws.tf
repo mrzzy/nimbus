5042532a07d7a4c726f8f0697f71ebee57ebe82d
Zhu Zhanyan <program.nom@gmail.com>
2023-03-26 15:10:05 +0800

    feat(terraform): add redshift serverless namespace & workgroup as data warehouse

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -24,3 +24,42 @@ resource "aws_s3_bucket_public_access_block" "lake" {
   block_public_acls   = true
   block_public_policy = true
 }
+
+# Redshift Serverless Data warehouse
+# iam policy to that allows read access to data lake
+data "aws_iam_policy_document" "allow_lake" {
+  statement {
+    sid = "AllowS3ReadOnlyOnLake"
+    resources = [
+      aws_s3_bucket.lake.arn
+    ]
+    # derived from predefined policy "AmazonS3ReadOnlyAccess"
+    actions = [
+      "s3:Get*",
+      "s3:List*",
+      "s3-object-lambda:Get*",
+      "s3-object-lambda:List*",
+    ]
+  }
+}
+# role to identify redshift when accessing other AWS services (eg. S3)
+resource "aws_iam_role" "warehouse" {
+  name               = "warehouse"
+  assume_role_policy = data.aws_iam_policy_document.allow_lake.json
+}
+# namespace to segeregate our db objects within the redshift serverless
+resource "aws_redshiftserverless_namespace" "warehouse" {
+  namespace_name       = "main"
+  db_name              = "mrzzy"
+  default_iam_role_arn = aws_iam_role.warehouse.arn
+}
+# workgroup of redshift serverless compute resources
+resource "aws_redshiftserverless_workgroup" "warehouse" {
+  namespace_name = aws_redshiftserverless_namespace.warehouse.id
+  workgroup_name = "main"
+  # by default, redshift serverless runs with 128 RPUs, which is overkill.
+  # with our small use case the minimum of 8 RPUs should do.
+  base_capacity = 8
+  # public access needed for querying from GCP over the internet
+  publicly_accessible = true
+}
