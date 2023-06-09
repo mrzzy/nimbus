ee5f25baf78ae49efd284a7e0da7f7f982d4e4b4
Zhu Zhanyan <program.nom@gmail.com>
2023-06-09 17:09:05 +0800

    feat(terraform): add gcs bucket to store airflow pipeline logs
    give gke workers with gke iam service account storage object admin
    permissions on that bucket.

--- terraform/modules/gcp/iam/variables.tf
+++ terraform/modules/gcp/iam/variables.tf
@@ -11,7 +11,7 @@ variable "project" {
 variable "allow_gh_actions" {
   type        = list(string)
   description = <<-EOF
-    Github repostories with Github Actions to create Workload Identity Pools for.
+    Github repositories with Github Actions to create Workload Identity Pools for.
 
     This allows the use of Github OIDC tokens issued to Action Workflows to beta
     as credentials for authenticate with GCP.
@@ -19,4 +19,13 @@ variable "allow_gh_actions" {
     Repositories should be specified in the format: "<OWNER>/<REPO>".
   EOF
   default     = []
+}
+
+variable "pipeline_logs_bucket" {
+  type        = string
+  description = <<-EOF
+    Name of the GCS bucket that will stores pipeline logs.
+    Gives Storage Object Admin IAM permissions to GKE workers to write
+    pipeline logs to this bucket.
+  EOF
 }
