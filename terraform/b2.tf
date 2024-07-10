b6d3ecdd96cadc0c4dfb2d4d82147d99b098cb15
Zhu Zhanyan <program.nom@gmail.com>
2024-07-10 15:03:58 +0800

    refactor(terraform): split Backblaze b2 infra to b2.tf

---
+++ terraform/b2.tf
@@ -0,0 +1,43 @@
+#
+# Nimbus
+# Terraform Deployment
+# Backblaze B2 Cloud Storage
+#
+
+
+# Backblaze B2 Cloud Storage provider
+provider "b2" {}
+# off-site backup location for pickle (laptop)
+resource "b2_bucket" "backup_pickle" {
+  bucket_name = "${local.domain_slug}-backup-pickle"
+  bucket_type = "allPrivate"
+
+  default_server_side_encryption {
+    algorithm = "AES256"
+    mode      = "SSE-B2"
+  }
+
+  lifecycle_rules {
+    # apply to all files
+    file_name_prefix             = ""
+    days_from_hiding_to_deleting = 1
+  }
+}
+
+# Data Lake: raw and staging data
+# used by mrzzy/providence project
+resource "b2_bucket" "data_lake" {
+  bucket_name = "${local.domain_slug}-data-lake"
+  bucket_type = "allPrivate"
+
+  default_server_side_encryption {
+    algorithm = "AES256"
+    mode      = "SSE-B2"
+  }
+
+  lifecycle_rules {
+    # apply to all files
+    file_name_prefix             = ""
+    days_from_hiding_to_deleting = 1
+  }
+}
